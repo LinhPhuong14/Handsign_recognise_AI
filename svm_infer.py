efc23723dd467a30e310b0c5bc2bd28fa63b0f89
Linh Phuong Nguyen <167509270+LinhPhuong14@users.noreply.github.com>
2024-08-21 03:49:36 +0700

    feat: take photo from camera and detect character

--- svm_infer.py
+++ svm_infer.py
@@ -41,3 +41,75 @@ def process(image, live=True):
     if live:
         cv2.imshow('MediaPipe Hands', cv2.flip(footage, 1))
     return data
+
+characters = ['A',
+ 'B',
+ 'C',
+ 'D',
+ 'E',
+ 'F',
+ 'G',
+ 'H',
+ 'I',
+ 'K',
+ 'L',
+ 'M',
+ 'N',
+ 'O',
+ 'P',
+ 'Q',
+ 'R',
+ 'S',
+ 'T',
+ 'U',
+ 'V',
+ 'W',
+ 'X',
+ 'Y',
+ 'delete',
+ 'space'
+ ]
+
+def add_distance(landmarks):
+    data = []
+    data.append(eulidean_distance(landmarks[4], landmarks[0]))
+    data.append(eulidean_distance(landmarks[8], landmarks[0]))
+    data.append(eulidean_distance(landmarks[12], landmarks[0]))
+    data.append(eulidean_distance(landmarks[16], landmarks[0]))
+    data.append(eulidean_distance(landmarks[20], landmarks[0]))
+    data.append(eulidean_distance(landmarks[4], landmarks[8]))
+    data.append(eulidean_distance(landmarks[4], landmarks[12]))
+    data.append(eulidean_distance(landmarks[8], landmarks[12]))
+    data.append(eulidean_distance(landmarks[12], landmarks[16]))
+    data.append(eulidean_distance(landmarks[20], landmarks[16]))
+    data.append(eulidean_distance(landmarks[8], landmarks[16]))
+    data.append(eulidean_distance(landmarks[8], landmarks[20]))
+    data.append(eulidean_distance(landmarks[12], landmarks[20]))
+    data.append(eulidean_distance(landmarks[4], landmarks[12]))
+    data.append(eulidean_distance(landmarks[4], landmarks[16]))
+    data.append(eulidean_distance(landmarks[4], landmarks[20]))
+    return data
+svm_model = joblib.load('svm_model.pkl')
+
+cap = cv2.VideoCapture(0)
+count = 0
+while True:
+    start = timer()
+    ret, frame = cap.read()
+    if not ret:
+        print("Cannot read video stream")
+        break
+    data = np.array(process(frame)) 
+    if np.all(data == 0):
+        print("No hand detected")
+    else:
+        result = svm_model.predict_proba(data.reshape(1, -1))[0]
+        classes = np.argmax(result)
+        prob = result[classes]
+        print(characters[classes], end="")
+    stop = timer()
+    print(f' - Execution time: {"{:.4f}".format((stop-start)*1000)} ms - FPS: {int(1/(stop-start))}')
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()

9d872e0ae02e613d373c44128ae1dd642ac2d4c0
Linh Phuong Nguyen <167509270+LinhPhuong14@users.noreply.github.com>
2024-08-21 03:41:39 +0700

    feat: process image

--- svm_infer.py
+++ svm_infer.py
@@ -18,3 +18,26 @@ def eulidean_distance(landmarkA, landmarkB):
     B = np.array([landmarkB.x, landmarkB.y])
     distance = np.linalg.norm(A-B)
     return distance
+
+def process(image, live=True):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+    data = []
+    footage = np.zeros(image.shape)
+
+    if results.multi_hand_landmarks:
+        landmarks = results.multi_hand_landmarks[0].landmark 
+        data = add_distance(landmarks)
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                footage,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+    else:
+        data = [0 for i in range(0, 16)]
+    if live:
+        cv2.imshow('MediaPipe Hands', cv2.flip(footage, 1))
+    return data
